b500f6816c46fef577849a6b0ccfdd8c9d1ee52b
pacoraggio <pacoraggio@gmail.com>
2020-02-05 14:11:43 +0000

    New UI

--- ui.R
+++ ui.R
@@ -15,48 +15,59 @@ shinyUI(fluidPage(
 
     # Application title
     titlePanel("Melbourne House Prices"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("rooms", "Number of Rooms",
-                        choices = c("1","2","3","4","5","6","7 or more"),
-                        selected = "3"),
-            selectInput("barhrooms", "Number of Bathrooms: ",
-                        choices = c("0","1","2","3","4 or more"),
-                        selected = "1"),
-            selectInput("type", "Type of Residency: ",
-                        choices = c("House Cottage Villa", "Unit Duplex","Town house"),
-                        selected = "House/Cottage/Villa"),
-            selectInput("region",
-                        "Select a region: ",
-                        choices = c("Select a Region", 
-                                    "Northern Metropolitan", 
-                                    "Western Metropolitan", 
-                                    "Southern Metropolitan", 
-                                    "Eastern Metropolitan", 
-                                    "South-Eastern Metropolitan", 
-                                    "Eastern Victoria", 
-                                    "Northern Victoria", 
-                                    "Western Victoria",
-                                    "All"),
-                        selected = "Select a Region"
-                        )#,
-            # submitButton("Select")
-            
+    
+    #    hr(),
+    
+    fluidRow(
+        column(4,
+               h4("Number of Rooms"),
+               br(),
+               selectInput("rooms", "Number of Bedrooms",
+                           choices = c("1","2","3","4","5","6","7 or more"),
+                           selected = "3"),
+               selectInput("barhrooms", "Number of Bathrooms: ",
+                           choices = c("0","1","2","3","4 or more"),
+                           selected = "1")),
+        column(4,
+               h4("Parameters"),
+               br(),
+               selectInput("type", "Type of Residency: ",
+                           choices = c("House Cottage Villa", "Unit Duplex","Town house"),
+                           selected = "House/Cottage/Villa"),
+               selectInput("region",
+                           "Select a region: ",
+                           choices = c("Select a Region", 
+                                       "Northern Metropolitan", 
+                                       "Western Metropolitan", 
+                                       "Southern Metropolitan", 
+                                       "Eastern Metropolitan", 
+                                       "South-Eastern Metropolitan", 
+                                       "Eastern Victoria", 
+                                       "Northern Victoria", 
+                                       "Western Victoria",
+                                       "All"),
+                           selected = "Select a Region"
+               )
         ),
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotlyOutput("housesPlot"),
-            textOutput("summary"),
-            plotlyOutput("statsummary")
-            # fluidRow(
-            #     splitLayout(cellWidths = c("50%", "50%"), plotOutput("plotgraph1"), plotOutput("plotgraph2"))
-            # fluidRow(
-            #     splitLayout(cellWidths = c("50%", "50%"), 
-            #                 plotlyOutput("globalstat"), 
-            #                 plotlyOutput("statsummary"))
-            # )
-        )
-    )
+        column(4,
+               h4("Summary Data"),
+               textOutput("summary"))
+        
+        
+    ),
+    br(),
+    fluidRow(
+        column(8,
+               plotlyOutput("housesPlot")),
+        column(4,
+               plotlyOutput("statsummary"))
+        # splitLayout(cellWidths = c("70%", "30%"), 
+        #             plotlyOutput("housesPlot"), 
+        #             plotlyOutput("statsummary"))
+    ),
+    # plotlyOutput("housesPlot"),
+    br(),
+    
+    #,
+    #)
 ))
