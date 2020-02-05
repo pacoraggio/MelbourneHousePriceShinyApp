a89da26329a3f156e04be0c4b0fb85783ae97aec
pacoraggio <pacoraggio@gmail.com>
2020-02-05 12:34:18 +0000

    add_trace to boxplot

--- plotboxScript.R
+++ plotboxScript.R
@@ -15,17 +15,27 @@ region <- c("Select a Region",
 n.rooms <- 4
 n.bath <- 2
 t.type <- "h"
-r.region <- region[2] 
 
 df.mel1 <- df.mel[which(df.mel$Rooms == n.rooms &
                             df.mel$Bathroom == n.bath &
                             df.mel$Type == t.type),]   
-
-df.mel2 <- df.mel[which(df.mel$Rooms == n.rooms &
+r.region <- region[9] 
+if(r.region == "Select a Region")
+   {
+       print(r.region)
+    } else if (r.region == "All")
+    {
+    print(r.region)
+    df.mel2 <- df.mel[which(df.mel$Rooms == n.rooms &
+                                df.mel$Bathroom == n.bath &
+                                df.mel$Type == t.type),]   
+    }else
+        {
+            df.mel2 <- df.mel[which(df.mel$Rooms == n.rooms &
                             df.mel$Bathroom == n.bath &
                             df.mel$Type == t.type &
                             df.mel$Regionname == r.region),]   
-
+}
 
 p <- plot_ly(y = ~df.mel1$Price, 
             type = "box") %>%
